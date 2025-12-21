c21230f9de8295f270ddd508c37c1a5bf771506a
Ziang Xie <xieziang0814@gmail.com>
2025-12-21 15:56:54 +0800

    Add files via upload

---
+++ locations.js
@@ -0,0 +1,205 @@
+const MAP_DATA = [
+  {
+    "name": "金沙湾",
+    "lat": 1.2844069,
+    "lng": 103.8656105,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "济州国际机场",
+    "lat": 33.5064232,
+    "lng": 126.5019613,
+    "category": "flight",
+    "layer": "2025"
+  },
+  {
+    "name": "獅子山",
+    "lat": 22.354455,
+    "lng": 114.182681,
+    "category": "hiking",
+    "layer": "2025"
+  },
+  {
+    "name": "中山大學",
+    "lat": 23.099234,
+    "lng": 113.2986084,
+    "category": "study",
+    "layer": "2025"
+  },
+  {
+    "name": "九龍城寨",
+    "lat": 22.3315241,
+    "lng": 114.1901067,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "唐寨山",
+    "lat": 25.4974469,
+    "lng": 118.233563,
+    "category": "hiking",
+    "layer": "2025"
+  },
+  {
+    "name": "三班镇",
+    "lat": 25.4480683,
+    "lng": 118.2969539,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "赤水镇",
+    "lat": 25.6415747,
+    "lng": 118.1364667,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "盖德镇",
+    "lat": 25.5048349,
+    "lng": 118.1818824,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "香港大學",
+    "lat": 22.2851975,
+    "lng": 114.1365048,
+    "category": "study",
+    "layer": "2025"
+  },
+  {
+    "name": "惠州霞新村",
+    "lat": 22.7696959,
+    "lng": 114.6607977,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "維多利亞港",
+    "lat": 22.2943996,
+    "lng": 114.173751,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "南方科技大学",
+    "lat": 22.5940934,
+    "lng": 113.9981199,
+    "category": "study",
+    "layer": "2025"
+  },
+  {
+    "name": "珠海日月贝",
+    "lat": 22.2807177,
+    "lng": 113.5798004,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "泉州文庙",
+    "lat": 24.9054345,
+    "lng": 118.5917502,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "德化一中",
+    "lat": 25.4939185,
+    "lng": 118.2396096,
+    "category": "study",
+    "layer": "2025"
+  },
+  {
+    "name": "海心桥",
+    "lat": 23.1093205,
+    "lng": 113.3177696,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "西归浦市",
+    "lat": 33.248096,
+    "lng": 126.5622619,
+    "category": "travel",
+    "layer": "2025"
+  },
+  {
+    "name": "城山日出峰",
+    "lat": 33.4656963,
+    "lng": 126.9330726,
+    "category": "hiking",
+    "layer": "2025"
+  },
+  {
+    "name": "沙罗岳",
+    "lat": 33.3713663,
+    "lng": 126.5714743,
+    "category": "hiking",
+    "layer": "2025"
+  },
+  {
+    "name": "香港国际机场",
+    "lat": 22.3224805,
+    "lng": 113.9133827,
+    "category": "flight",
+    "layer": "2025"
+  },
+  {
+    "name": "晋江国际机场",
+    "lat": 24.7997404,
+    "lng": 118.5941557,
+    "category": "flight",
+    "layer": "2025"
+  },
+  {
+    "name": "樟宜机场",
+    "lat": 1.3582201,
+    "lng": 103.9890641,
+    "category": "flight",
+    "layer": "2025"
+  },
+  {
+    "name": "蓬莱国际机场",
+    "lat": 37.6618201,
+    "lng": 120.9880689,
+    "category": "flight",
+    "layer": "2026"
+  },
+  {
+    "name": "首都机场",
+    "lat": 40.070813,
+    "lng": 116.6057197,
+    "category": "flight",
+    "layer": "2026"
+  },
+  {
+    "name": "江北国际机场",
+    "lat": 29.7187404,
+    "lng": 106.6412907,
+    "category": "flight",
+    "layer": "2026"
+  },
+  {
+    "name": "高崎国际机场",
+    "lat": 24.5397344,
+    "lng": 118.1315299,
+    "category": "flight",
+    "layer": "2026"
+  },
+  {
+    "name": "萧山国际机场",
+    "lat": 30.2359903,
+    "lng": 120.43888,
+    "category": "flight",
+    "layer": "2023"
+  },
+  {
+    "name": "杭州师范大学",
+    "lat": 30.2887516,
+    "lng": 120.0099184,
+    "category": "study",
+    "layer": "2023"
+  }
+];
